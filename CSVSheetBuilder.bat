4c6fc11d4844962c127fd805beb1d28cd87928e0
BryanRSummit <bryan@reddsummit.com>
2024-07-10 14:55:58 -0600

    Adding a .bat

---
+++ CSVSheetBuilder.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+"C:\Python312\python.exe" "C:\Users\%USERNAME%\Desktop\CSV Sheet Builder\main.py"
+exit
+
+@REM C:\Users\%USERNAME%\Documents\ReddSummitApps\NumberManipulator_GUI\build\exe.win-amd64-3.12\NumberManipulatorGUI.exe
